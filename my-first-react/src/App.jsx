604f63143d5e7d01a5705044bae6a0cb12ae3811
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-08 06:30:31 +0400

    App.jsx added list

--- my-first-react/src/App.jsx
+++ my-first-react/src/App.jsx
@@ -7,18 +7,17 @@ function greeting() {
 	);
 }
 
+let animals = ["Dog", "Cat", "Lion", "Cow", "Lizard"];
+
+let animalList = animals.map((animal) => <li key={animal}>{animal}</li>);
+
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
 		<>
-			<h1>Test title</h1>
-			<svg>
-				<circle cx="150" cy="75" r="20" stroke="green" strokeWidth="2" />
-			</svg>
-			<form>
-				<input type="text" />
-			</form>
+			<h1>Animals</h1>
+			<ul>{animalList}</ul>
 			{greeting()}
 		</>
 	);
