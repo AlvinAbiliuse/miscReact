ed4dec1626c5c9dbe8745b305ac075f5616764e1
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-08 05:21:12 +0400

    App.jsx

--- my-first-react/src/App.jsx
+++ my-first-react/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 
+function greeting() {
+	return (
+		<h1>&quot;I swear by my pretty floral bonnet, I will end you.&quot;</h1>
+	);
+}
+
 function App() {
 	const [count, setCount] = useState(0);
 
-	return (
-		<>
-			<p>Hello World!</p>
-		</>
-	);
+	return <>{greeting()}</>;
 }
 
 export default App;
