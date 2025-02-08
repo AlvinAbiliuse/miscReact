3df48cd68ff48f8a683aad3a46aba366bece81b6
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-09 03:11:29 +0400

    App.jsx

--- reactPractice/src/App.jsx
+++ reactPractice/src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
 import "./App.css";
 
-function Button(click) {
+function Button(props) {
 	return (
-		<button>{click.click === "yes" ? "Click Me!" : "Don't Click Me!"}</button>
+		<button style={{ color: props.btnStyle }}>
+			{props.click === "yes" ? "Click Me!" : "Don't Click Me!"}
+		</button>
 	);
 }
 
 function App() {
 	return (
 		<>
-			<Button click="yes" />
-			<Button click="no" />
-			<Button click="yes" />
+			<Button click="yes" btnStyle="gray" />
+			<Button click="no" btnStyle="red" />
+			<Button click="yes" btnStyle="blue" />
 		</>
 	);
 }
