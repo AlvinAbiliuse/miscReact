aff6814cb64b08479dc199eaee60c6004aa11363
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-08 06:55:21 +0400

    progress

--- my-first-react/src/App.jsx
+++ my-first-react/src/App.jsx
@@ -12,10 +12,20 @@ function ListItem(props) {
 }
 
 function List(props) {
+	if (!props.animals) {
+		return <div>Loading...</div>;
+	}
+
+	if (props.animals.length === 0) {
+		return <div>There are no animals in the list</div>;
+	}
+
 	return (
 		<ul>
 			{props.animals.map((animal) => {
-				return animal.startsWith("L") ? <li key={animal}>{animal} </li> : null;
+				if (animal.length > 3) {
+					return <li key={animal}>{animal} </li>;
+				}
 			})}
 		</ul>
 	);
