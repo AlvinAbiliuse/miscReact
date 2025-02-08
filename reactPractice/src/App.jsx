585b192c856314ab5fe678057cb733470bab51c0
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-09 02:59:32 +0400

    shortened Button function

--- reactPractice/src/App.jsx
+++ reactPractice/src/App.jsx
@@ -1,20 +1,18 @@
 import { useState } from "react";
 import "./App.css";
 
-function Button() {
-	return <button>Click Me</button>;
-}
-
-function Button2() {
-	return <button>Don&apos;t Click Me!</button>;
+function Button(click) {
+	return (
+		<button>{click.click === "yes" ? "Click Me!" : "Don't Click Me!"}</button>
+	);
 }
 
 function App() {
 	return (
 		<>
-			<Button />
-			<Button2 />
-			<Button />
+			<Button click="yes" />
+			<Button click="no" />
+			<Button click="yes" />
 		</>
 	);
 }
