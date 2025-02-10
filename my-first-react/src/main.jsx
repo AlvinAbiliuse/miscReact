85bcc754be9dae6d3f046836256e377c367a4ecf
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-10 06:18:48 +0400

    src/*

--- my-first-react/src/main.jsx
+++ my-first-react/src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Greeting from "./Greeting.jsx";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<App />
-		<Greeting />
 	</StrictMode>
 );
