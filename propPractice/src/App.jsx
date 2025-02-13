5240cd8896367038511ca81ce3ab820200d3e17f
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-13 04:54:33 +0400

    App.jsx

--- propPractice/src/App.jsx
+++ propPractice/src/App.jsx
@@ -1,57 +1,69 @@
 import { getImageUrl } from "./utils.jsx";
 
+function Avatar({
+	name,
+	imgId,
+	width,
+	height,
+	alt,
+	profession,
+	awards,
+	awardsN,
+	discovered,
+}) {
+	return (
+		<section className="profile">
+			<h2>{name}</h2>
+			<img
+				className="avatar"
+				src={getImageUrl(imgId)}
+				alt={alt}
+				width={width}
+				height={height}
+			/>
+			<ul>
+				<li>
+					<b>Profession: </b>
+					{profession}
+				</li>
+				<li>
+					<b>Awards: {awardsN} </b> {awards}
+				</li>
+				<li>
+					<b>Discovered: </b> {discovered}
+				</li>
+			</ul>
+		</section>
+	);
+}
+
 export default function Gallery() {
 	return (
 		<div>
 			<h1>Notable Scientists</h1>
-			<section className="profile">
-				<h2>Maria Skłodowska-Curie</h2>
-				<img
-					className="avatar"
-					src={getImageUrl("szV5sdG")}
-					alt="Maria Skłodowska-Curie"
-					width={70}
-					height={70}
-				/>
-				<ul>
-					<li>
-						<b>Profession: </b>
-						physicist and chemist
-					</li>
-					<li>
-						<b>Awards: 4 </b>
-						(Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal,
-						Matteucci Medal)
-					</li>
-					<li>
-						<b>Discovered: </b>
-						polonium (chemical element)
-					</li>
-				</ul>
-			</section>
-			<section className="profile">
-				<h2>Katsuko Saruhashi</h2>
-				<img
-					className="avatar"
-					src={getImageUrl("YfeOqp2")}
-					alt="Katsuko Saruhashi"
-					width={70}
-					height={70}
-				/>
-				<ul>
-					<li>
-						<b>Profession: </b>
-						geochemist
-					</li>
-					<li>
-						<b>Awards: 2 </b>
-						(Miyake Prize for geochemistry, Tanaka Prize)
-					</li>
-					<li>
-						<b>Discovered: </b>a method for measuring carbon dioxide in seawater
-					</li>
-				</ul>
-			</section>
+
+			<Avatar
+				name="Maria Skłodowska-Curie"
+				imgId="szV5sdG"
+				alt="Maria Skłodowska-Curie"
+				width="70"
+				height="70"
+				profession="physicist and chemist"
+				awards=" (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)"
+				awardsN="4"
+				discovered="polonium (chemical element)"
+			/>
+
+			<Avatar
+				name="Katsuko Saruhashi"
+				imgId="YfeOqp2"
+				alt="Katsuko Saruhashi"
+				width="70"
+				height="70"
+				awards="(Miyake Prize for geochemistry, Tanaka Prize)"
+				awardsN="2"
+				discovered="a method for measuring carbon dioxide in seawater"
+			/>
 		</div>
 	);
 }
