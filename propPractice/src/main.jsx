722af0e727fc19abbb0fd67db1c9b914d25307b5
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-13 05:13:40 +0400

    added SecondApp.jsx to main.jsx

--- propPractice/src/main.jsx
+++ propPractice/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import SecondApp from "./SecondApp.jsx";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
+		<SecondApp />
 		<App />
 	</StrictMode>
 );
