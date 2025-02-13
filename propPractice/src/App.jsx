4ccb034a2a08f9053b8ff0d149ccff7b53393078
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-13 04:32:53 +0400

    progress

--- propPractice/src/App.jsx
+++ propPractice/src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { getImageUrl } from "./utils.js";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function Gallery() {
+	return (
+		<div>
+			<h1>Notable Scientists</h1>
+			<section className="profile">
+				<h2>Maria Skłodowska-Curie</h2>
+				<img
+					className="avatar"
+					src={getImageUrl("szV5sdG")}
+					alt="Maria Skłodowska-Curie"
+					width={70}
+					height={70}
+				/>
+				<ul>
+					<li>
+						<b>Profession: </b>
+						physicist and chemist
+					</li>
+					<li>
+						<b>Awards: 4 </b>
+						(Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal,
+						Matteucci Medal)
+					</li>
+					<li>
+						<b>Discovered: </b>
+						polonium (chemical element)
+					</li>
+				</ul>
+			</section>
+			<section className="profile">
+				<h2>Katsuko Saruhashi</h2>
+				<img
+					className="avatar"
+					src={getImageUrl("YfeOqp2")}
+					alt="Katsuko Saruhashi"
+					width={70}
+					height={70}
+				/>
+				<ul>
+					<li>
+						<b>Profession: </b>
+						geochemist
+					</li>
+					<li>
+						<b>Awards: 2 </b>
+						(Miyake Prize for geochemistry, Tanaka Prize)
+					</li>
+					<li>
+						<b>Discovered: </b>a method for measuring carbon dioxide in seawater
+					</li>
+				</ul>
+			</section>
+		</div>
+	);
 }
-
-export default App
