437e55e2ff12cb2c90758e4750b025aa755a0a89
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-15 03:51:11 +0400

    App.jsx

--- moreReact/src/App.jsx
+++ moreReact/src/App.jsx
@@ -1,9 +1,44 @@
-
+import { useState } from "react";
+import { initialLetters } from "./data.jsx";
+import Letter from "./Letter.jsx";
 
 export default function App() {
+	const [letters, setLetters] = useState(initialLetters);
+	const [highlightedLetter, setHighlightedLetter] = useState(null);
+
+	function handleHover(letter) {
+		setHighlightedLetter(letter);
+	}
+
+	function handleStar(starred) {
+		setLetters(
+			letters.map((letter) => {
+				if (letter.id === starred.id) {
+					return {
+						...letter,
+						isStarred: !letter.isStarred,
+					};
+				} else {
+					return letter;
+				}
+			})
+		);
+	}
+
 	return (
 		<>
-			<h1>The Start</h1>
+			<h2>Inbox</h2>
+			<ul>
+				{letters.map((letter) => (
+					<Letter
+						key={letter.id}
+						letter={letter}
+						isHighlighted={letter === highlightedLetter}
+						onHover={handleHover}
+						onToggleStar={handleStar}
+					/>
+				))}
+			</ul>
 		</>
-	)
-};
+	);
+}
