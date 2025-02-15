a3618f5b085b5ace3eb6ebeb9124e16d1360a52e
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-15 05:40:02 +0400

    progress

--- moreReact/src/main.jsx
+++ moreReact/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Mail from "./mail/Mail.jsx";
+import Multiple from "./multiple/Multiple.jsx";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
+		<Multiple />
 		<Mail />
 	</StrictMode>
 );
