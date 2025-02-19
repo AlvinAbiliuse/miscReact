c581d486cbfaeb32bf62dda8d0065df15668d0c0
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-20 03:41:43 +0400

    main.jsx

--- moreReact/src/main.jsx
+++ moreReact/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Mail from "./mail/Mail.jsx";
 import Multiple from "./multiple/Multiple.jsx";
+import Broken from "./broken/Broken.jsx";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
+		<div class="container">
+			<Broken />
+		</div>
 		<div class="container">
 			<Multiple />
 		</div>
